2f7be38a163b7880db36b40f51d9a2db43f1671e
Thiago Sordi <thiagosordi10@gmail.com>
2020-07-15 17:32:11 -0300

    Exclusão de todos assets/maps

--- core/assets/maps/castle_env.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="castle_env" tilewidth="16" tileheight="16" tilecount="2560" columns="64">
- <image source="castle_env.png" width="1024" height="640"/>
-</tileset>
--- core/assets/maps/top_far_bgrnd.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="top_far_bgrnd" tilewidth="16" tileheight="16" tilecount="2560" columns="40">
- <image source="top_far_bgrnd.png" width="640" height="1024"/>
-</tileset>
