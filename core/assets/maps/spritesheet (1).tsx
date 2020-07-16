f1a405e761bc076899fd39a3d5f43b55a91d0e1b
GuilhermePretto <62142257+GuilhermePretto@users.noreply.github.com>
2020-07-16 15:25:42 -0300

    erro

--- core/assets/maps/spritesheet (1).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="spritesheet (1)" tilewidth="16" tileheight="16" tilecount="24192" columns="192">
- <image source="../Downloads/spritesheet (1).png" width="3074" height="2020"/>
-</tileset>
