4ea13970865ecf16b2435f5e37563add274f922b
GuilhermePretto <62142257+GuilhermePretto@users.noreply.github.com>
2020-07-29 15:06:08 -0300

    map ato 2

---
+++ core/assets/maps/tiledAto2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="tiledAto2" tilewidth="16" tileheight="16" tilecount="27648" columns="192">
+ <image source="tileAto1.png" width="3078" height="2307"/>
+</tileset>
