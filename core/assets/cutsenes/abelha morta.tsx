daf7462e35ef7408649299a9a70e6cea8295f978
GuilhermePretto <62142257+GuilhermePretto@users.noreply.github.com>
2020-07-28 21:58:56 -0300

    cutsenes

---
+++ core/assets/cutsenes/abelha morta.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="abelha morta" tilewidth="16" tileheight="16" tilecount="36" columns="6">
+ <image source="../../../../../../Downloads/anthon andar-5.png (1).png" width="100" height="100"/>
+</tileset>
---
+++ core/assets/cutsenes/abelhas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="abelhas" tilewidth="16" tileheight="16" tilecount="529" columns="23">
+ <image source="../../../../../../Downloads/anthon pular (9).png" width="375" height="375"/>
+</tileset>
