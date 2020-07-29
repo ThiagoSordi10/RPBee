b9de1894f5d2861fca15cea972c3b9a4d2d4f44f
GuilhermePretto <62142257+GuilhermePretto@users.noreply.github.com>
2020-07-29 14:15:37 -0300

    cutsenes finais

---
+++ core/assets/cutsenes/fruteira.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="fruteira" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="../../../../../../Downloads/fruteira (1).png" width="175" height="175"/>
+</tileset>
