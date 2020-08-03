358971a80317dd105c19c0ebbfebdb9adb8e0d11
Thiago Sordi <thiagosordi10@gmail.com>
2020-08-03 13:58:05 -0300

    Update MainMenuScreen.java

--- core/src/com/rpbee/Screens/MainMenuScreen.java
+++ core/src/com/rpbee/Screens/MainMenuScreen.java
@@ -89,7 +89,7 @@ public class MainMenuScreen implements Screen{
         btnPlay.addListener( new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new CutsceneScreen(game));
+                game.setScreen(new PlayScreen(game));
                 dispose();
             };
         });
